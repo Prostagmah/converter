92c5790c762d676975ca3b149b96816276f622da
Артур <Suppermen1012@gmail.com>
2022-03-25 13:16:29 +0300

    25.03.2022 12:45

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Location = new System.Drawing.Point(161, 265);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(434, 20);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(138, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(508, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Хочешь узнать Курс?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(291, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Перевести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(125, 117);
+            this.panel1.TabIndex = 3;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(15, 95);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(64, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Шекель";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.Click += new System.EventHandler(this.Curse_click_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 72);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(55, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Тенге";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.Curse_click_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Биткоин";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.Curse_click_CheckedChanged);
+            this.radioButton3.Click += new System.EventHandler(this.Curse_click_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Евро";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.Curse_click_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Доллар";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.Curse_click_CheckedChanged);
+            this.radioButton1.Click += new System.EventHandler(this.Curse_click_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 310);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(434, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(602, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(605, 316);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "₽";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.No;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(605, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Курс актуален на 25.03.2022 (12:45)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    double curs = double.Parse(TextBox.Text) * 99.00;
+                    textBox2.Text = curs.ToString();
+                }
+                else if (TextBox.Text == "")
+                {
+                    double cursRub = double.Parse(textBox2.Text) / 99.00;
+                    TextBox.Text = cursRub.ToString();
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    double curs = double.Parse(TextBox.Text) * 144.77;
+                    textBox2.Text = curs.ToString();
+                }
+                else if (TextBox.Text == "")
+                {
+                    
+                        double cursRub = double.Parse(textBox2.Text) / 144.77;
+                        TextBox.Text = cursRub.ToString();
+                }
+            }
+            else if (radioButton3.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    double curs = double.Parse(TextBox.Text) * 4360000.88;
+                    textBox2.Text = curs.ToString();
+                }
+                else if (TextBox.Text == "")
+                {
+                    double cursRub = double.Parse(textBox2.Text) / 4360000.88;
+                    TextBox.Text = cursRub.ToString();
+                }
+            }
+            else if (radioButton4.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    double curs = double.Parse(TextBox.Text) * 0.20;
+                    textBox2.Text = curs.ToString();
+                }
+                else if (TextBox.Text == "")
+                {
+                    double cursRub = double.Parse(textBox2.Text) / 0.20;
+                    TextBox.Text = cursRub.ToString();
+                }
+            }
+            else if (radioButton5.Checked)
+            {
+                if (textBox2.Text == "")
+                {
+                    double curs = double.Parse(TextBox.Text) * 30.59;
+                    textBox2.Text = curs.ToString();
+                }
+                else if (TextBox.Text == "")
+                {
+                    double cursRub = double.Parse(textBox2.Text) / 30.59;
+                    TextBox.Text = cursRub.ToString();
+                }
+            }
+        }
+
+        private void Curse_click_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Curse_click_Click(object sender, EventArgs e)
+        {
+            RadioButton r = (RadioButton)sender;
+            switch (r.Name)
+            {
+                case "radioButton1":
+                    label2.Text = "Доллар";
+                    break;
+                case "radioButton2":
+                    label2.Text = "Евро";
+                    break;
+                case "radioButton3":
+                    label2.Text = "Биткоин";
+                    break;
+                case "radioButton4":
+                    label2.Text = "Тенге";
+                    break;
+                case "radioButton5":
+                    label2.Text = "Шекель";
+                    break;
+            }
+        }
+    }
+}
